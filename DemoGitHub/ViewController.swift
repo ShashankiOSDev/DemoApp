d9f7c8af340b9decf3742ec71fc52b1863148193
Shashank Singh <shashanksingh@shashanks-macbook.local>
2016-01-25 18:48:37 +0530

    Initial Commit

---
+++ DemoGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoGitHub
+//
+//  Created by Shashank Singh on 25/01/16.
+//  Copyright © 2016 S. S. Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
